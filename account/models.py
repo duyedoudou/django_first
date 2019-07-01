292f0f124c75c6b43bf6e413011324b6db0893e1
杜晔 <1019737754@qq.com>
2019-07-01 19:47:27 +0800

    article_pb
    实现简单文章发布的功能

--- account/models.py
+++ account/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
+
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,unique=True)
     birth = models.DateField(blank=True,null=True)
     phone = models.CharField(max_length=20,null=True)
+
+    def __str__(self):
+        return 'user {}'.format(self.user.username)
 
 # 创建个人信息相关的数据模型类
 class UserInfo(models.Model):
@@ -17,7 +23,7 @@ class UserInfo(models.Model):
     aboutme = models.TextField(blank=True)
     photo = models.ImageField(blank=True)
 
-
     def __str__(self):
         return 'user {}'.format(self.user.username)
+
 
