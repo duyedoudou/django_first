7736c35639a179f91394b777e606a4d0472e9655
杜晔 <1019737754@qq.com>
2019-07-05 14:14:14 +0800

    MD
    增加markdown,文章标题列表，及可简单查看文章

--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
@@ -73,6 +73,15 @@ def article_post(request):
                       {'article_post_form':article_post_form,
                        'article_columns':article_columns})
 
+@login_required(login_url='/account/login')
+def article_list(request):
+    articles = ArticlePost.objects.filter(author=request.user)
+    return render(request,'article/column/article_list.html',{'articles':articles})
+
+@login_required(login_url='/account/login')
+def article_detail(request,id,slug):
+    article = get_object_or_404(ArticlePost,id=id,slug=slug)
+    return render(request,'article/column/article_detail.html',{'article':article})
 
 
 
